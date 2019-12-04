008439dc97940667e21fa6ee3f46d45ff74d3cc5
YuliiaChupryna <julia.chuprina2907@gmail.com>
2019-12-04 11:36:52 +0200

    Updates

---
+++ precache-manifest.e820c814315b2eed49410bc3a7fd00b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe82626cb820f60c167759930461277b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b483e71c9257480f3dc9",
+    "url": "/static/css/main.733215be.chunk.css"
+  },
+  {
+    "revision": "d1bf606ea68b30240b50",
+    "url": "/static/js/2.a7975bab.chunk.js"
+  },
+  {
+    "revision": "b483e71c9257480f3dc9",
+    "url": "/static/js/main.4334cf52.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
